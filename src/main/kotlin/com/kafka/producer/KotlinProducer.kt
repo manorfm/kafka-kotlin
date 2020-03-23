3a00e97ba6d491bedd701a8a4419246e764bc3ba
Manoel Rodrigo Farinha de Medeiros <manorfm@hotmail.com>
2020-03-23 17:57:39 -0300

    iniciando repositorio

---
+++ src/main/kotlin/com/kafka/producer/KotlinProducer.kt
@@ -0,0 +1,12 @@
+package com.kafka.producer
+
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class KotlinProducer (private val kafkaTemplate: KafkaTemplate<String, String>) {
+
+    fun send(message: String) {
+        kafkaTemplate.send("simple-message-topic", message)
+    }
+}
---
+++ src/main/kotlin/com/kafka/rest/MessageApi.kt
@@ -0,0 +1,17 @@
+package com.kafka.rest
+
+import com.kafka.producer.KotlinProducer
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class MessageApi(private val kotlinProducer: KotlinProducer) {
+
+    @PostMapping("/message")
+    fun publish(@RequestBody message: String) {
+        kotlinProducer.send(message)
+    }
+}
